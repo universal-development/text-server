2c052816aa2bd46bce8a12d944952fa3bfef7fdf
denis <denis@universal-development.com>
2017-12-15 22:12:57 +0200

    Added spring boot template app

---
+++ textlines-server/src/main/java/com/unidev/textlines/TextlinesApplication.java
@@ -0,0 +1,12 @@
+package com.unidev.textlines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextlinesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextlinesApplication.class, args);
+	}
+}
